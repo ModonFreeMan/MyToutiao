d1a1ee01799a1c42d723610945c3c0af4b942f92
Wang Weiqi <1329735742@qq.com>
2026-05-30 16:54:33 +0800

    test: stabilize feed player widget playback

--- test/widget/feed/feed_player_test.dart
+++ test/widget/feed/feed_player_test.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:video_player_platform_interface/video_player_platform_interface.dart';
 
 import 'package:video_player_mvp/app/app.dart';
 import 'package:video_player_mvp/features/player/controllers/player_controller.dart';
 
+import '../../helpers/fake_video_player_platform.dart';
 import '../../helpers/test_app.dart';
 
 void main() {
+  setUp(() {
+    VideoPlayerPlatform.instance = FakeVideoPlayerPlatform();
+  });
+
   Finder findVerticalFeedPageView() {
     return find.byWidgetPredicate(
       (widget) => widget is PageView && widget.scrollDirection == Axis.vertical,
@@ -17,6 +23,7 @@ void main() {
   Future<void> flingToNextPage(WidgetTester tester) async {
     await tester.fling(findVerticalFeedPageView(), const Offset(0, -500), 1000);
     await tester.pump(const Duration(milliseconds: 700));
+    await tester.pump();
   }
 
   testWidgets('starts player for video item and stops on image item', (
@@ -30,11 +37,13 @@ void main() {
       UncontrolledProviderScope(container: container, child: const App()),
     );
 
-    await tester.pump(const Duration(milliseconds: 400));
+    await tester.pump(const Duration(milliseconds: 700));
 
-    expect(container.read(playerControllerProvider).videoId, 'video_001');
+    final playerState = container.read(playerControllerProvider);
+    expect(playerState.videoId, 'video_001');
+    expect(playerState.isInitialized, isTrue);
+    expect(playerState.isPlaying, isTrue);
     expect(find.byType(Slider), findsOneWidget);
-    expect(find.byType(CircularProgressIndicator), findsWidgets);
     expect(find.byIcon(Icons.play_arrow_rounded), findsNothing);
 
     await flingToNextPage(tester);
